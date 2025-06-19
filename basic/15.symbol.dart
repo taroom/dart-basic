c7081aaf75635aa428fc21659d0e878a6216b18f
Sultan Room <sultanroomcode@gmail.com>
2025-06-19 16:40:00 +0700

    [ROM] Add initial implementation of symbol data type example

---
+++ basic/15.symbol.dart
@@ -0,0 +1,8 @@
+void main() {
+  // symbol adalah tipe data yang bisa digunakan sebagai konstanta
+  Symbol s1 = #helloTaroom;
+  Symbol s2 = Symbol("hello Taroom");
+
+  print(s1);
+  print(s2);
+}
