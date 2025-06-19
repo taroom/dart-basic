0f632c6eb11a6c855cb8fb1c1de54b8c71646d1a
Sultan Room <sultanroomcode@gmail.com>
2025-06-19 19:52:47 +0700

    [ROM] Enhance null safety example with default values and late initialization

--- basic/18.null-safety.dart
+++ basic/18.null-safety.dart
@@ -6,4 +6,23 @@ void main() {
   if (age != null) {
     print(age.toDouble());
   }
+
+  // default value dengan ??
+  String? namamu;
+  print(namamu ?? "Tidak ada nama yang diberikan");
+
+  // tidak disarankan
+  // int? umur;
+  // double? bmg = umur?.toDouble();
+  // print(bmg); // ini akan menghasilkan null jika umur adalah null
+
+  // menggunakan ! untuk menandakan bahwa nilai tidak akan null
+  // int? angka;
+  // print(angka!.toDouble()); // ini tidak aman karena angka bisa saja null
+
+  // menggunakan late untuk menunda inisialisasi
+  late String nama;
+  nama = "John Doe";
+  print(
+      nama); // ini aman karena nama sudah terisi dengan nilai sebelum digunakan
 }
