0d86925af689d41df1f1b0b23e97539dddbda6f5
Sultan Room <sultanroomcode@gmail.com>
2025-06-19 18:05:06 +0700

    [ROM] Add example demonstrating null data type usage in Dart

---
+++ basic/17.null-data-type.dart
@@ -0,0 +1,14 @@
+void main() {
+  // null is a special data type in Dart that represents the absence of a value.
+  // String fullname; // fullname harus diassign / diisi
+  // print(fullname); // This will cause an error because fullname is not initialized.
+  String? name; // name can be null
+  int? age; // age can be null
+  double? height; // height can be null
+  bool? isEmployed; // isEmployed can be null
+
+  print(name);
+  print(age);
+  print(height);
+  print(isEmployed);
+}
