9e2b85a3ccb0c096e5dbf7436ae23a9e79d3335d
Sultan Room <sultanroomcode@gmail.com>
2025-06-21 13:44:53 +0700

    [ROM] Refactor closure example to improve readability and structure

--- basic/23.closure.dart
+++ basic/23.closure.dart
@@ -1,7 +1,17 @@
+// Closure adalah fungsi yang dapat mengakses variabel dari scope luar
+// fungsi ini dapat mengakses variabel yang dideklarasikan di luar fungsi tersebut
+// tapi masih dalam scope yang sama
 void main() {
   int counter = 1;
   void incrementCounter() {
     counter++; // mengakses variabel counter dari scope luar
+
+    void incrementInnerCounter() {
+      counter++; // mengakses variabel counter dari scope luar
+    }
+
+    incrementInnerCounter();
+    incrementInnerCounter();
   }
 
   incrementCounter(); // memanggil fungsi untuk increment
