062e6b58b7ccfc027a44a9935f0e7bcfcc32d889
Sultan Room <sultanroomcode@gmail.com>
2025-06-21 21:52:19 +0700

    [ROM] Add Person class with redirecting constructors for flexible initialization

---
+++ oop/9.redirecting-constructor.dart
@@ -0,0 +1,32 @@
+// Definisi class Person
+class Person {
+  String? name;
+  int age = 0;
+
+  Person(this.name, this.age) {}
+
+  // Redirecting constructor
+  Person.withName(String nama) : this(nama, 0);
+  Person.withAge(int umur) : this('Unknown', umur);
+  Person.denganNamaDanUmur(int umur, String namamu) : this(namamu, umur);
+}
+
+void main() {
+  // Buat objek manusia1, dan isi constructornya
+  var manusia1 = Person('Budi', 30);
+  print(manusia1.name);
+  print(manusia1.age);
+
+  // Buat objek manusia1, dan isi constructornya
+  var manusia2 = Person.withName('Taroom');
+  print(manusia2.name);
+  print(manusia2.age);
+
+  var manusia3 = Person.withAge(35);
+  print(manusia3.name);
+  print(manusia3.age);
+
+  var manusia4 = Person.denganNamaDanUmur(40, 'Dalton');
+  print(manusia4.name);
+  print(manusia4.age);
+}
