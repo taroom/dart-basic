84ab709d319be076c442b199f176604f49a66fb6
Sultan Room <sultanroomcode@gmail.com>
2025-06-23 13:32:50 +0700

    [ROM] Add Category class and example usage in main function

---
+++ oop/21.import.dart
@@ -0,0 +1,7 @@
+import 'data-import/category.dart';
+
+void main() {
+  // Importing a class from another file
+  var category = Category('10', 'Electronics');
+  print('Category: ${category.name}');
+}
